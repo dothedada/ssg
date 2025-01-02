31affa7338c74e22df6bc01f1ce52a2b40c9bb8d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-02 12:20:20 -0500

    fixes htmlnode init, props_to_html and repr methods

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,25 +1,39 @@
 class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
+        if tag == None and value == None and children == None and props == None:
+            raise Exception("HTMLNode cannot be empty")
         self.tag = tag
         self.value = value
-        self.children = children
-        self.props = props
+        if isinstance(children, list):
+            self.children = children
+        else:
+            self.children = None
+        if isinstance(props, dict):
+            self.props = props
+        else:
+            self.props = None
 
     def to_html(self):
         raise NotImplementedError()
 
     def props_to_html(self):
-        element_props = {}
+        if not self.props:
+            return "No props to parse"
+        element_props = ""
         for prop in self.props:
             element_props += f' {prop}="{self.props[prop]}"'
         return element_props
 
     def __repr__(self):
-        return (
-            "HTMLNode {\n"
-            f"\ttag: {self.tag}\n"
-            f"\tvalue: {self.value}\n"
-            f"\tchildren: {self.children}\n"
-            f"\tprops: {self.props_to_html()}\n"
-            "}"
-        )
+        repr = "HTMLNode {\n"
+        if self.tag:
+            repr += f"\ttag: {self.tag}\n"
+        if self.value:
+            repr += f"\tvalue: {self.value}\n"
+        if self.children:
+            repr += f"\tchildren: {self.children}\n"
+        if self.props:
+            repr += f"\tprops: {self.props_to_html()}\n"
+        repr += "}"
+
+        return repr
