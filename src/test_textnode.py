e81ec66cdbc08d0922f2c34aaf76b04adeee8eae
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-02 20:48:55 -0500

    Update test fot TextNode and create test fot text_node_to_html_node function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +23,55 @@ class TestTextNode(unittest.TestCase):
         node_a = TextNode("link node", TextType.LINKS, "https://mmejia.com")
         node_b = TextNode("other link node", "links", "https://cv.mmejia.com")
         self.assertNotEqual(node_a, node_b)
+
+    def test_TextType_validation_exeption(self):
+        with self.assertRaises(ValueError):
+            TextNode("test", "wrongType")
+
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_only_accepst_TextNode(self):
+        with self.assertRaises(TypeError):
+            text_node_to_html_node("noup")
+
+    def test_none_input(self):
+        with self.assertRaises(TypeError):
+            text_node_to_html_node(None)
+
+    def test_create_raw_text_node(self):
+        text_node = TextNode("sample text", TextType.NORMAL_TEXT)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "")
+        self.assertEqual(html_node.value, "sample text")
+
+    def test_create_bold_text_node(self):
+        text_node = TextNode("sample bold text", TextType.BOLD_TEXT)
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "sample bold text")
+
+    def test_create_italic_text_node(self):
+        text_node = TextNode("sample italic text", "italic")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "sample italic text")
+
+    def test_create_link_text_node(self):
+        text_node = TextNode("sample link text", "links", "https://cv.mmejia.com")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "sample link text")
+        self.assertTrue('href="https://cv.mmejia.com"' in html_node.to_html())
+
+    def test_create_image_text_node(self):
+        text_node = TextNode(
+            "this is the alt text", TextType.IMAGE, "https://this.is.the/source/text"
+        )
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertTrue('alt="this is the alt text"' in html_node.to_html())
+        self.assertTrue('src="https://this.is.the/source/text"' in html_node.to_html())
 
 
 if __name__ == "__main__":
