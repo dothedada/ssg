60ba456f5e78a844efd62af1a714d5d2527f3e42
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-02 19:31:57 -0500

    Adds text_node_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -7,7 +8,7 @@ class TextType(Enum):
     ITALIC_TEXT = "italic"
     CODE_TEXT = "code"
     LINKS = "links"
-    IMAGES = "images"
+    IMAGE = "images"
 
 
 class TextNode:
@@ -32,3 +33,27 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match (text_node.text_type):
+        case TextType.NORMAL_TEXT:
+            return LeafNode(tag="", value=text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINKS:
+            return LeafNode(
+                tag="a", value=text_node.text, props={"href": text_node.url}
+            )
+        case TextType.IMAGE:
+            return LeafNode(
+                tag="img",
+                value="",
+                props={"src": text_node.url, "alt": text_node.value},
+            )
+        case _:
+            raise Exception("Invalid TextNode")
