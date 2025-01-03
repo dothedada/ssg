fe15e79e1f8cb5a6d9e1c07832da786d60748ed2
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-03 17:27:16 -0500

    create unit test for links and images splitter

--- src/test_textnodesdelimiter.py
+++ src/test_textnodesdelimiter.py
@@ -3,68 +3,70 @@ from splitnodesdelimiter import (
     split_nodes_delimiter,
     extract_markdown_images,
     extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
 )
 from textnode import TextNode, TextType
 
 
 class TestNodesSplitter(unittest.TestCase):
     def test_right_assignation_with_word_in_middle(self):
-        text_node = TextNode("text **bold** more text", TextType.NORMAL_TEXT)
-        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD_TEXT)
+        text_node = TextNode("text **bold** more text", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD)
         self.assertEqual(len(new_nodes), 3)
-        self.assertTrue(new_nodes[1].text_type == TextType.BOLD_TEXT)
+        self.assertTrue(new_nodes[1].text_type == TextType.BOLD)
 
     def test_right_assignation_with_word_at_start(self):
-        text_node = TextNode("**bold** more text", TextType.NORMAL_TEXT)
-        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD_TEXT)
+        text_node = TextNode("**bold** more text", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD)
         self.assertEqual(len(new_nodes), 2)
-        self.assertTrue(new_nodes[0].text_type == TextType.BOLD_TEXT)
+        self.assertTrue(new_nodes[0].text_type == TextType.BOLD)
 
     def test_right_assignation_with_word_at_end(self):
-        text_node = TextNode("some text **bold**", TextType.NORMAL_TEXT)
-        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD_TEXT)
+        text_node = TextNode("some text **bold**", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD)
         self.assertEqual(len(new_nodes), 2)
-        self.assertTrue(new_nodes[1].text_type == TextType.BOLD_TEXT)
+        self.assertTrue(new_nodes[1].text_type == TextType.BOLD)
 
     def test_multiple_styled_texts(self):
         text_node = TextNode(
-            "text *italic* more text, *another italic*", TextType.NORMAL_TEXT
+            "text *italic* more text, *another italic*", TextType.NORMAL
         )
-        new_nodes = split_nodes_delimiter([text_node], "*", TextType.ITALIC_TEXT)
+        new_nodes = split_nodes_delimiter([text_node], "*", TextType.ITALIC)
         self.assertEqual(len(new_nodes), 4)
-        self.assertTrue(new_nodes[1].text_type == TextType.ITALIC_TEXT)
-        self.assertTrue(new_nodes[3].text_type == TextType.ITALIC_TEXT)
+        self.assertTrue(new_nodes[1].text_type == TextType.ITALIC)
+        self.assertTrue(new_nodes[3].text_type == TextType.ITALIC)
 
     def test_handle_multiple_TextNode(self):
-        text_node_a = TextNode("text `code`, more text", TextType.NORMAL_TEXT)
-        text_node_b = TextNode("more text `more code`.", TextType.NORMAL_TEXT)
+        text_node_a = TextNode("text `code`, more text", TextType.NORMAL)
+        text_node_b = TextNode("more text `more code`.", TextType.NORMAL)
         new_nodes = split_nodes_delimiter(
-            [text_node_a, text_node_b], "`", TextType.CODE_TEXT
+            [text_node_a, text_node_b], "`", TextType.CODE
         )
         self.assertEqual(len(new_nodes), 6)
-        self.assertTrue(new_nodes[1].text_type == TextType.CODE_TEXT)
-        self.assertTrue(new_nodes[4].text_type == TextType.CODE_TEXT)
+        self.assertTrue(new_nodes[1].text_type == TextType.CODE)
+        self.assertTrue(new_nodes[4].text_type == TextType.CODE)
 
     def test_mantain_the_base_TextType(self):
-        text_node = TextNode("text *italic*, more text", TextType.BOLD_TEXT)
-        new_nodes = split_nodes_delimiter([text_node], "*", TextType.ITALIC_TEXT)
+        text_node = TextNode("text *italic*, more text", TextType.BOLD)
+        new_nodes = split_nodes_delimiter([text_node], "*", TextType.ITALIC)
         self.assertEqual(len(new_nodes), 3)
-        self.assertTrue(new_nodes[0].text_type == TextType.BOLD_TEXT)
-        self.assertTrue(new_nodes[1].text_type == TextType.ITALIC_TEXT)
-        self.assertTrue(new_nodes[2].text_type == TextType.BOLD_TEXT)
+        self.assertTrue(new_nodes[0].text_type == TextType.BOLD)
+        self.assertTrue(new_nodes[1].text_type == TextType.ITALIC)
+        self.assertTrue(new_nodes[2].text_type == TextType.BOLD)
 
     def test_raise_error_unpaired_delimiters(self):
-        text_node = TextNode("text **bold, more text", TextType.NORMAL_TEXT)
+        text_node = TextNode("text **bold, more text", TextType.NORMAL)
         with self.assertRaises(Exception):
-            split_nodes_delimiter([text_node], "**", TextType.BOLD_TEXT)
+            split_nodes_delimiter([text_node], "**", TextType.BOLD)
 
     def test_ignores_empty_delimiters(self):
-        text_node = TextNode("text ** **, more text", TextType.NORMAL_TEXT)
-        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD_TEXT)
+        text_node = TextNode("text ** **, more text", TextType.NORMAL)
+        new_nodes = split_nodes_delimiter([text_node], "**", TextType.BOLD)
         self.assertEqual(len(new_nodes), 1)
 
     def text_empty_nodes_list_return_empty_list(self):
-        new_nodes = split_nodes_delimiter([], "**", TextType.BOLD_TEXT)
+        new_nodes = split_nodes_delimiter([], "**", TextType.BOLD)
         self.assertIsInstance(new_nodes, list)
         self.assertEqual(len(new_nodes), 0)
 
@@ -147,3 +149,187 @@ class ExtractMarkdownLink(unittest.TestCase):
         text = "text ![link_name](https://url.com/1)"
         link_links = extract_markdown_links(text)
         self.assertEqual(len(link_links), 0)
+
+
+class SplitNodesForImages(unittest.TestCase):
+    def test_TextNode_without_images(self):
+        node = TextNode("no images in this text", TextType.NORMAL)
+        self.assertEqual(split_nodes_image([node]), [node])
+
+    def test_TextNode_with_one_image_at_middle(self):
+        node = TextNode("one links ![alt_text](www.url.com).", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("one links ", TextType.NORMAL),
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode(".", TextType.NORMAL),
+            ],
+        )
+
+    def test_TextNode_with_one_image_at_start(self):
+        node = TextNode("![alt_text](www.url.com), text.", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode(", text.", TextType.NORMAL),
+            ],
+        )
+
+    def test_TextNode_with_one_image_at_end(self):
+        node = TextNode("some text ![alt_text](www.url.com)", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("some text ", TextType.NORMAL),
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_images(self):
+        node = TextNode(
+            "text ![alt_text](www.url.com)more text![alt_text2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("text ", TextType.NORMAL),
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode("more text", TextType.NORMAL),
+                TextNode("alt_text2", TextType.IMAGE, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_images_text_between(self):
+        node = TextNode(
+            "![alt_text](www.url.com) more text ![alt_text2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode(" more text ", TextType.NORMAL),
+                TextNode("alt_text2", TextType.IMAGE, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_images_no_text_between(self):
+        node = TextNode(
+            "![alt_text](www.url.com)![alt_text2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode("alt_text2", TextType.IMAGE, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_mixed_content(self):
+        node = TextNode(
+            "![alt_text](www.url.com) and [link](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_image([node]),
+            [
+                TextNode("alt_text", TextType.IMAGE, "www.url.com"),
+                TextNode(" and [link](www.url2.com)", TextType.NORMAL),
+            ],
+        )
+
+
+class SplitNodesForLinks(unittest.TestCase):
+    def test_TextNode_without_links(self):
+        node = TextNode("no links in this text", TextType.NORMAL)
+        self.assertEqual(split_nodes_link([node]), [node])
+
+    def test_TextNode_with_one_link_at_middle(self):
+        node = TextNode("one [link](www.url.com).", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("one ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode(".", TextType.NORMAL),
+            ],
+        )
+
+    def test_TextNode_with_one_link_at_start(self):
+        node = TextNode("[link](www.url.com), text.", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode(", text.", TextType.NORMAL),
+            ],
+        )
+
+    def test_TextNode_with_one_link_at_end(self):
+        node = TextNode("some text [link](www.url.com)", TextType.NORMAL)
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("some text ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "www.url.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_links(self):
+        node = TextNode(
+            "text [link](www.url.com)more text[link2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("text ", TextType.NORMAL),
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode("more text", TextType.NORMAL),
+                TextNode("link2", TextType.LINK, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_links_text_between(self):
+        node = TextNode(
+            "[link](www.url.com) more text [link2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode(" more text ", TextType.NORMAL),
+                TextNode("link2", TextType.LINK, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_multiple_images_no_text_between(self):
+        node = TextNode(
+            "[link](www.url.com)[link2](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode("link2", TextType.LINK, "www.url2.com"),
+            ],
+        )
+
+    def test_TextNode_with_mixed_content(self):
+        node = TextNode(
+            "[link](www.url.com) and ![alt_text](www.url2.com)",
+            TextType.NORMAL,
+        )
+        self.assertEqual(
+            split_nodes_link([node]),
+            [
+                TextNode("link", TextType.LINK, "www.url.com"),
+                TextNode(" and ![alt_text](www.url2.com)", TextType.NORMAL),
+            ],
+        )
