a758c71877f16a07d128c8e66d084efdb9a8cdb1
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-04 17:34:17 -0500

    Fix the capture pattern for markdown blocks

--- src/blocks.py
+++ src/blocks.py
@@ -1,10 +1,19 @@
+import re
+
+
 def markdown_to_blocks(markdown):
+    pattern = r"\n[ \t]*\n"
     blocks = []
 
-    for section in markdown.split("\n\n"):
-        if section.strip("\n ") == "":
+    for section in re.split(pattern, markdown):
+        if section.strip(" \t\n") == "":
             continue
         else:
-            blocks.append(section)
+            blocks.append(section.strip(" \t\n"))
 
     return blocks
+
+
+def block_to_block_type(block):
+    pattern = r"^(#{1,6}|```|>|[*-]|\d+\.) *\w"
+    print(re.match(pattern, block))
