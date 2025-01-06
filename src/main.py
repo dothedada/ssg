d9742b95ce72aec110bafbbc4da7f0a59e4419ed
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-06 13:05:33 -0500

    updates main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode
 from filehandler import static_files_handler
+from pagemaker import generate_page
 
 
 def main():
     static_files_handler()
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
