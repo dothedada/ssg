70de49d28b037932e70c048a0b3ba8a108d0d24a
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-01-06 11:16:00 -0500

    add static files and updates main.py

--- src/main.py
+++ src/main.py
@@ -3,9 +3,7 @@ from filehandler import static_files_handler
 
 
 def main():
-    test_node = TextNode("Entonces pendejo", "code", "https://mmejia.com")
     static_files_handler()
-    print(test_node)
 
 
 main()
